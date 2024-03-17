e4e98723197a508d6cf0e983ecd0c9969e86923e
sugandha-kadp <piyumsugandha@gmail.com>
2024-03-17 21:28:52 +0530

    remove unused imports in spring

--- model_retraining_backend/demo/src/main/java/com/example/demo/GoogleTestController.java
+++ model_retraining_backend/demo/src/main/java/com/example/demo/GoogleTestController.java
@@ -1,11 +1,9 @@
 package com.example.demo;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeoutException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
